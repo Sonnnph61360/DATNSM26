9b5ce664a9b7bc15a9162cda3542a5bb5f7d3a1f
vinh170606-hub <vinh170606@gmail.com>
2026-08-01 01:55:00 +0700

    fix router login,register

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,26 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ClientLayout from "./layouts/ClientLayout";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 font-sans text-gray-800 flex flex-col">
-      <Header />
-
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/detail" element={<Detail />} />
-        </Routes>
-      </main>
-
-      <Footer />
+    <>
+      <Routes>
+        <Route path="/*" element={<ClientLayout />}>
+          <Route index element={<Home />} />
+          <Route path="detail" element={<Detail />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
       <Toaster />
-    </div>
+    </>
   );
 }
 
