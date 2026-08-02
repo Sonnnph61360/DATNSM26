28e358960df1571dd98a390fc2ff5e7a52b83516
Tu <nguyentu0813@gmail.com>
2026-08-03 00:14:34 +0700

    fix-router

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,8 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import Booking from "./pages/Booking";
+import List from "./pages/List";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Blog from "./pages/Blog";
+import FieldPage from "./pages/FieldPage";
+import AdminLayout from "./layouts/AdminLayout";
+import AdminBookings from "./pages/AdminBookings";
+import Dashboard from "./pages/admin/Dashboard";
+import Courts from "./pages/admin/Courts";
+import CalendarPage from "./pages/admin/CalendarPage";
 import ClientLayout from "./layouts/ClientLayout";
 
 function App() {
@@ -13,9 +22,22 @@ function App() {
         <Route path="/*" element={<ClientLayout />}>
           <Route index element={<Home />} />
           <Route path="detail" element={<Detail />} />
+          <Route path="booking" element={<Booking />} />
+          <Route path="tim-san" element={<List />} />
+          <Route path="fields" element={<FieldPage />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
         </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          {/* <Route path="calendar" element={<CalendarPage />} />
+          <Route path="bookings" element={<AdminBookings />} />
+          <Route path="courts" element={<Courts />} /> */}
+        </Route>
       </Routes>
       <Toaster />
     </>
