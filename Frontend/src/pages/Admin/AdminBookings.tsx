d4dc511ef7ce5f91f99c29e1c6d6f02e33a7d71b
nguyenxuanhuy03 <huy708070@gmail.com>
2026-09-22 23:50:20 +0700

    fixx

--- Frontend/src/pages/Admin/AdminBookings.tsx
+++ Frontend/src/pages/Admin/AdminBookings.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { Table, Select, message, Spin, Button, Input, Modal, Form, DatePicker, TimePicker, InputNumber, Divider } from "antd";
 import { QrCode, Filter, CheckCircle2, CreditCard, Banknote, RefreshCcw, Download, Plus, Zap } from "lucide-react";
 import { api, type Booking, formatCurrency, formatSlotRange, Court } from "../../lib/api";
 import * as XLSX from 'xlsx';
 import { formatDateVi } from "../../lib/locale";
-import { Html5QrcodeScanner } from "html5-qrcode";
-import { QRScanner } from ".././QRScanner"; // Chỉnh lại đường dẫn tới file QRScanner.tsx 
+
 export default function AdminBookings() {
   const [bookings, setBookings] = useState<Booking[]>([]);
   const [loading, setLoading] = useState(true);
